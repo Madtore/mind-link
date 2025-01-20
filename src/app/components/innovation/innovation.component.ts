abce1f8cecb763f9d622a182de86619f3db5fea2
Madtore <73850831+Madtore@users.noreply.github.com>
2025-01-20 11:44:38 +0100

    [Fix] add style en de home componetes

---
+++ src/app/components/innovation/innovation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-innovation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './innovation.component.html',
+  styleUrl: './innovation.component.scss'
+})
+export class InnovationComponent {
+}
