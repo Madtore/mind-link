b95c738c92118985d20df751bafaa28edbc10056
Madtore <73850831+Madtore@users.noreply.github.com>
2025-01-21 11:01:46 +0100

    [Features] add botton for goback and for start questionary

--- src/app/pages/doctor-details/doctor-details.component.ts
+++ src/app/pages/doctor-details/doctor-details.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-details',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ CommonModule, RouterLink ],
   templateUrl: './doctor-details.component.html',
   styleUrl: './doctor-details.component.scss'
 })
 export class DoctorDetailsComponent implements OnInit{
 
   psychologist = history.state.psychologist;
-  // destructurar psychologist para tener los valores 
-
+ 
   details = {
     name : this.psychologist.name,
     specialization : this.psychologist.specialization,
--- src/app/pages/doctors/doctors.component.ts
+++ src/app/pages/doctors/doctors.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { PsychologistListComponent } from '../../components/psychologist-list/psychologist-list.component';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [ PsychologistListComponent],
+  imports: [ PsychologistListComponent, HeroComponent],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.scss'
 })
