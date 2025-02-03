56fdff222b3eb77380770307d87bba24d468b71d
Dani <sanchezgarciad485@gmail.com>
2025-02-03 12:21:10 +0100

    formulario registro actualizado con las opciones extras al selecionar medico

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  userType: string = ''; // 'paciente' o 'medico'
 
+  // Modelo del formulario
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    dateOfBirth: '',
+    gender: '',
+    specialization: '',
+    licenseNumber: '',
+    priceHour: null
+  };
+
+  onUserTypeChange(type: string): void {
+    this.userType = type;
+
+    // Si cambia a paciente, limpiamos los datos extra de médico
+    if (type === 'paciente') {
+      this.formData.specialization = '';
+      this.formData.licenseNumber = '';
+      this.formData.priceHour = null;
+    }
+  }
+
+  onSubmit(): void {
+    if (this.userType === 'medico') {
+      console.log('Datos enviados (Médico):', this.formData);
+    } else {
+      console.log('Datos enviados (Paciente):', this.formData);
+    }
+  }
 }
